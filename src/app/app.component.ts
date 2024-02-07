f8c06b782f1aee80cdfce3f9844ffb5e1d85dff8
zaid-butt <zaid-butt@users.noreply.github.com>
2024-02-07 21:46:33 +0500

    ztest1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
+
+/**
+ * @title Drag&Drop sorting
+ */
 
 @Component({
   selector: 'app-root',
+  templateUrl: 'app.html',
+  styleUrls: ['app.css'],
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CdkDropList, CdkDrag],
 })
 export class AppComponent {
-  title = 'Manage Menu';
+  
+movies = [
+  {
+    uId: '0',
+    name: 'GroupA',
+    children: ["a1", "a2", "a3"]
+  },
+  {
+    uId: '3',
+    name: 'GroupB',
+    children: ["b1", "b2"]
+  },
+  {
+    uId: '4',
+    name: 'GroupC',
+    children: ["c1", "c2", "c3", "c4"]
+  }
+];
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
 }
