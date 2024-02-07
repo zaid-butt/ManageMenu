c8d68a7b2ca2e20fe592c02bad19523a68b861f0
zaid-butt <zaid-butt@users.noreply.github.com>
2024-02-08 02:35:21 +0500

    backup 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import {Component} from '@angular/core';
 import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
+import {MatButtonModule} from '@angular/material/button';
 
-/**
- * @title Drag&Drop sorting
- */
+import { header } from './components/header/header.component';
+import { AlertsComponent } from './components/alerts/alerts.component';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.html',
-  styleUrls: ['app.css'],
-  standalone: true,
-  imports: [CdkDropList, CdkDrag],
+    selector: 'app-root',
+    templateUrl: 'app.html',
+    styleUrls: ['app.css'],
+    standalone: true,
+    imports: [CdkDropList, CdkDrag, header , AlertsComponent, MatButtonModule]
 })
 export class AppComponent {
   
@@ -18,17 +19,18 @@ export class AppComponent {
   {
     Id: '0',
     Category: 'Burgers',
-    Items: ["Cheese Burger", "chicken burger", "Turkey burger", "Veggie burger"]
+    Items: ["Cheese Burger", "Chicken burger", "Veggie burger"]
   },
   {
     Id: '3',
     Category: 'Pizza',
-    Items: ["Cheese Pizza", "Veggie Pizza", "Pepperoni Pizza", "Meat Pizza", "BBQ Chicken Pizza", "Supreme Pizza"]
+    Items: ["Cheese Pizza", "Pepperoni Pizza", "Meat Pizza", "BBQ Chicken Pizza", "Supreme Pizza"]
   },
   {
     Id: '4',
     Category: 'Donuts',
-    Items: ["Baked Raspberry", "Mochi Donuts", "Bavarian Cream", "Chocolate Donuts"]
+    //Items: ["Baked Raspberry", "Bavarian Cream", "Chocolate Donuts"]
+    Items: []
   }
 ];
 
---
+++ src/app/components/alerts/alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+//import { DangerTriangle } from './DangerTriangle.svg';
+
+@Component({
+  selector: 'app-alerts',
+  standalone: true,
+  imports: [],
+  templateUrl: './alerts.component.html',
+  styleUrl: './alerts.component.css'
+})
+export class AlertsComponent {
+
+}
